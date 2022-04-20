3ba0feb132d0dfb51f0679c2bfd99642218b5ae5
Demian <learc47@gmail.com>
2022-04-20 01:16:07 -0400

    init

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,32 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="text-center">
+    <h1 class="py-3">Crear una nueva tarea</h1>
+    <label class="pr-4">Tarea</label>
+    <input class="me-4 ms-4" type="text" v-model= "duties" placeholder="Ingrese nueva tarea"/>
+    <button class="btn btn-dark btn-lg" @click="Agregar">Crear</button>
+    <ul>
+      <h2>Lista</h2>
+      <li v-for="(Tarea, $index) in Tareas" :key="$index">
+        {{Tarea}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "app",
+  data: () => ({
+    duties: "",
+    Tareas: ["Compras supermercado", "Botar la basura", "Pagar netflix"]
+  }),
+  methods:{
+    Agregar: function(){
+      this.Tareas.push(this.duties)
+      this.duties = "";
+    }
   }
-}
+};
 </script>
 
 <style>
